25de042b19f8d4ea30d483c22ba475d5c8dd7476
nitishjha74 <kumarnitishjha8877@gmail.com>
2025-05-13 20:20:39 +0530

    authentication_veryfied_from_jsjauth_project

---
+++ business/authentication.py
@@ -0,0 +1,46 @@
+from rest_framework.authentication import BaseAuthentication
+from rest_framework.exceptions import AuthenticationFailed
+import requests
+from django.conf import settings
+from django.contrib.auth.models import AnonymousUser
+
+class SSOBusinessTokenAuthentication(BaseAuthentication):
+    def authenticate(self, request):
+        auth_header = request.headers.get("Authorization")
+        if not auth_header or not auth_header.startswith("Token "):
+            return None
+
+        token = auth_header.split("Token ")[1]
+
+        try:
+            response = requests.post(
+                settings.AUTH_SERVER_URL + "/verify-token/",
+                json={"token": token},
+                timeout=5
+            )
+            if response.status_code != 200:
+                raise AuthenticationFailed("Invalid or expired token.")
+
+            data = response.json()
+            user = AuthenticatedBusinessUser(
+                id=data["user_id"],
+                business_id=data["business_id"],
+                business_name=data["business_name"]
+            )
+
+            return (user, None)
+        except requests.RequestException:
+            raise AuthenticationFailed("Authentication service unreachable.")
+        
+        
+        
+
+class AuthenticatedBusinessUser:
+    def __init__(self, id, business_id, business_name):
+        self.id = id
+        self.business_id = business_id
+        self.business_name = business_name
+        self.is_authenticated = True  # Required by DRF
+
+    def __str__(self):
+        return f"BusinessUser {self.business_id}"
--- business/urls.py
+++ business/urls.py
@@ -6,6 +6,7 @@ from . import views
 
 urlpatterns = [
 
+path("business-reward-rules/", views.BusinessRewardRuleListCreateApi.as_view(), name="business-reward-rules"),
     
 
     
